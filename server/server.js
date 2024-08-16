023e7229d3245152fab6fbb5f700d3833020a37c
Rafael Bastos <47896406+rmpbastos@users.noreply.github.com>
2024-08-15 20:50:33 -0700

    cors

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://books-3380-vercel.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 // MongoDB connection
